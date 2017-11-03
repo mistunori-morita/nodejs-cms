67f1b78ea121503a6b9f1a1028187087a9c51234
morita mitunori <mitukun32@gmail.com>
2017-11-03 15:23:36 +0900

    routes/ admin_page.js fix

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-// Get pages index
+// GEt Page modeles
+var Page = require('../models/page');
 
+// Get pages index
 router.get('/', function(req,res){
     res.send('admin area');
 });
@@ -19,6 +21,59 @@ router.get('/add-page', function(req,res){
       slug: slug,
       content: content
     });
+
+});
+
+
+// Post add page
+router.post('/add-page', function(req,res){
+
+    req.checkBody('title', 'Title must have a value.').notEmpty();
+    req.checkBody('content', 'Content must have a value.').notEmpty();
+
+
+    var title = req.body.title;
+    var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
+    if(slug == '') slug = title.replace(/\s+/g, '-').toLowerCase();
+    var content = req.body.content;
+
+    var errors = req.validationErrors();
+
+    if(errors) {
+      res.render('admin/add_page',{
+        errors: errors,
+        title: title,
+        slug: slug,
+        content: content
+      });
+    }else{
+      Page.findOne({slug: slug}, function(err, page){
+        if(page){
+           req.flash('danger', 'Page slug exists, choose anotehr,');
+           res.render('admin/add_page',{
+             errors: errors,
+             title: title,
+             slug: slug,
+             content: content
+           });
+        }else{
+          var Page = new Page ({
+            title: title,
+            slug: slug,
+            content: content,
+            sorting: 0
+          });
+
+          Page.save(function(err){
+            if(err) return console.log(err);
+
+            req.flash('success', 'Page added!');
+            res.redirect('/admin/pages');
+          });
+        }
+      });
+    }
+
 
 });
 
